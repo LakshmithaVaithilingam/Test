4b115717ba4fb211268060aabbdc9d4a4fb63687
yasarakithmini <123257361+yasarakithmini@users.noreply.github.com>
2023-09-11 13:18:19 +0530

    Update TaskInput.js

--- to-do-list-app/src/TaskInput/TaskInput.js
+++ to-do-list-app/src/TaskInput/TaskInput.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../index.css'; // Import the index.css file
+//import '../index.css'; // Import the index.css file
 
 
 function TaskInput({ addTask }) {
